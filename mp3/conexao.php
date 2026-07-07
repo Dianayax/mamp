9c5c192d2ad9187ce6d3b1edc2fe2c25fe1b2c95
Diana Iara Faria Silva <diana17faria@gmail.com>
2026-07-07 11:03:27 +0100

    i

--- mp3/conexao.php
+++ mp3/conexao.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $db_name = 'mp3player';
-$username = 'root';
-$password = ''; 
+$username = 'Diana';
+$password = 'jrA37@f3W_0g7M@'; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
